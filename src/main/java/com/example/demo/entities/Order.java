5203683b43a3f2f44251b48bfd1e8f088a24f324
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2020-11-11 15:26:32 -0400

    entidade Pedido, datas como Instant e padrão ISO 8601 - parte 1

---
+++ src/main/java/com/example/demo/entities/Order.java
@@ -0,0 +1,89 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity /* anotação que indica que a classe corresponde a uma tabela do BD */
+@Table(name = "tb_order") /* renomeando a tabela para evitar conflito com a palavras reservada Order do SQL */
+public class Order implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant instant;
+	
+	@ManyToOne /* tipo de relacionamento: muitos para um */
+	@JoinColumn(name = "client_id") /* adicão de um coluna */
+	private User client;
+	
+	public Order() {
+		super();
+	}
+
+	public Order(Long id, Instant instant, User client) {
+		super();
+		this.id = id;
+		this.instant = instant;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getInstant() {
+		return instant;
+	}
+
+	public void setInstant(Instant instant) {
+		this.instant = instant;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
