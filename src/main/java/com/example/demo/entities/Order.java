2742aeffe98ae0bd37366c0992c0e7e3cbeeaa74
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2020-11-12 15:00:51 -0400

    Entidade OrderItem. Associação muitos-para-muitos com dados extras. - Parte 2

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -2,6 +2,8 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.demo.entities.enums.OrderStatus;
@@ -32,6 +35,9 @@ public class Order implements Serializable {
 	@ManyToOne /* tipo de relacionamento: muitos para um */
 	@JoinColumn(name = "client_id") /* adicão de um coluna */
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 		super();
@@ -78,6 +84,14 @@ public class Order implements Serializable {
 		if (orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<OrderItem> items) {
+		this.items = items;
 	}
 
 	@Override
---
+++ src/main/java/com/example/demo/respositories/OrderItemRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.respositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.OrderItem;
+
+/* Classe responsável por acessar os dados no BD.
+ * Implementa os padrões de injeção de dependência.
+ * */
+@Repository /* registra a classe como repositório, para que o Spring faça a injeção de dependência.
+No entanto, a anotação é opcional, pois sua superclasse já está registrada */
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
